eac813e84b85fd184d446b11a985c7a69c48382c
Robert Gilmore <Robert.Warren.Gilmore@gmail.com>
2014-10-17 14:30:34 -0400

    Wrote Logger.

---
+++ src/canddproject1/Logger.java
@@ -0,0 +1,20 @@
+package canddproject1;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Logger {
+
+    /**
+     * Provides standardised logging for actions.
+     * @param actorName Who is doing the action?
+     * @param actionName What is the action?
+     */
+    public static void logAction(String actorName, String actionName) {
+        Date currentDate = SimulationClock.currentSimulationDate();
+        String timeOfDay = new SimpleDateFormat("HH:mm").format(currentDate);
+        String message = timeOfDay + " " + actorName + " " + actionName + ".";
+        System.out.println(message);
+    }
+
+}
