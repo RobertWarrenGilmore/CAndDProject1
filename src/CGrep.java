66010852fc62e35622e1373cae4768d4d169a799
Robert Gilmore <Robert.Warren.Gilmore@gmail.com>
2014-10-31 13:46:14 -0400

    Wrote GrepCallable.

--- src/CGrep.java
+++ src/CGrep.java
@@ -1,9 +1,13 @@
 import java.util.Arrays;
+import java.util.concurrent.Callable;
 
 public class CGrep {
 
     public static void main(String[] args){
         String pattern = args[0];
         String[] fileNames = Arrays.copyOfRange(args, 1, args.length);
+        for (String fileName: fileNames) {
+            // TODO I dunno. -Robert
+        }
     }
 }
---
+++ src/GrepCallable.java
@@ -0,0 +1,13 @@
+import java.util.concurrent.Callable;
+
+public class GrepCallable implements Callable<Found> {
+
+    public GrepCallable(String fileName, String pattern) {
+        
+    }
+
+    @Override
+    public Found call() throws Exception {
+        return null;
+    }
+}
