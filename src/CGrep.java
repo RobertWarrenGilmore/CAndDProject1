7af8827fdc5dd0a6233002e804ad2c2defe1c4dd
Robert Gilmore <Robert.Warren.Gilmore@gmail.com>
2014-10-31 13:37:22 -0400

    Caught arguments.

--- src/CGrep.java
+++ src/CGrep.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
 
 public class CGrep {
 
     public static void main(String[] args){
-        
+        String pattern = args[0];
+        String[] fileNames = Arrays.copyOfRange(args, 1, args.length);
     }
 }
