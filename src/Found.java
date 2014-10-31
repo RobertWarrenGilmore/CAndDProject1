4e11df1be4142ac6d76e9d85dacb9590e2ad4775
Robert Gilmore <Robert.Warren.Gilmore@gmail.com>
2014-10-31 13:31:24 -0400

    Wrote Found.

---
+++ src/Found.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Found {
+    private String fileName;
+    private String[] lines;
+
+    public Found(String fileName, String[] lines) {
+        this.fileName = fileName;
+        this.lines = lines;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public List<String> getLines() {
+        return Arrays.asList(lines);
+    }
+}
