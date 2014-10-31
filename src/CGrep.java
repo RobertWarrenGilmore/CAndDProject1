caf1cccb8d36d1d6d630a69bdfedf9972e0975a4
cmcgorrill <cmm5215@rit.edu>
2014-10-31 14:01:26 -0400

    Executor and CompletionService

--- src/CGrep.java
+++ src/CGrep.java
@@ -1,13 +1,29 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.concurrent.Callable;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class CGrep {
 
+	 private void solve(Executor e, Collection<Callable<Found>> solvers) throws InterruptedException, ExecutionException {
+	     CompletionService<Found> ecs = new ExecutorCompletionService<Found>(e);
+	     for (Callable<Found> s : solvers)
+	         ecs.submit(s);
+	     int n = solvers.size();
+	     for (int i = 0; i < n; ++i) {
+	         Found f = ecs.take().get();
+	         if (f != null) {
+	             //TODO do something with Found... print all matching lines?
+	         }
+	     }
+	 }
+	
     public static void main(String[] args){
         String pattern = args[0];
         String[] fileNames = Arrays.copyOfRange(args, 1, args.length);
         for (String fileName: fileNames) {
-            // TODO I dunno. -Robert
+            //solve();
         }
     }
 }
---
+++ src/ConcExecutor.java
@@ -0,0 +1,14 @@
+import java.util.concurrent.*;
+
+public class ConcExecutor implements Executor{
+
+	@Override
+	public void execute(Runnable arg0) {
+		// TODO Auto-generated method stub	
+	}
+	
+	public void submit(Callable<Found> c) {
+		//Manage the calls
+	}
+	
+}
